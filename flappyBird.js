5378cd222937074f213b7e336fb8afe23f35adb5
yomiojo001@gmail.com <yomiojo001@gmail.com>
2020-01-06 17:20:16 +0100

    gameover-commit

--- flappyBird.js
+++ flappyBird.js
@@ -58,8 +58,15 @@ window.onload = function draw(){
                 y: Math.floor(Math.random()*pipeNorth.height) - pipeNorth.height
             })
         }
+
+        // Gameover conditions
+        if(bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >= cvs.height - fg.height){
+            location.reload();
+        }
         
     }
+
+
 
     ctx.drawImage(fg, 0,cvs.height - fg.height);
     
